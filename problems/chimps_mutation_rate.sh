9510c97e3abb506f69988cb7375bc091894e20f4
alexpinch <alexpinch@hotmail.com>
2023-02-16 15:06:27 -0800

    Create chimps_mutation_rate.sh

---
+++ problems/chimps_mutation_rate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Alex Pinch, BIOL435 Feb. 2023
+# From G. Coop, Chapter 5, Question 1
+# For this and the next question, assume that humans and chimps split around 
+# 5.5x10^6 years ago, have a generation time of 20 years, that the speciation
+# occurred instantaneously in allopatry with no subsequent gene flow, and the
+# ancestral effective population size of the human and chimp common ancestor
+# population was 10,000 individuals. Nachman and Crowell sequenced 12 pseudogenes
+# in humans and chimps and found substitutions at 1.3% of the sites.
+# A) What is the mutation rate per site per generation at these genes?
+# B) All of the pseudogenes they sequenced are on the autosomes. What would your
+# prediction be for pseudogenes on the X and Y chromosomes, given that fewer mutations
+# occur in the female germline than in the male germline per generation.
